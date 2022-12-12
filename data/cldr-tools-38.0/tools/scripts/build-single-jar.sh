8e76a215943a1d8a9a8383a17837c7c564fcecc7
Behnam Esfahbod <behnam@zwnj.org>
2022-12-12 05:17:23 -0800

    Update data to version 38.1

--- data/cldr-tools-38.0/tools/scripts/build-single-jar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# This replaces cldr.jar with an assembly including dependencies.
-#
-
-HERE=$(dirname $0)
-set -x
-mvn --file=${HERE}/../pom.xml clean && mvn -pl java package assembly:single -DskipTests=true --file=${HERE}/../pom.xml 
-cp -v ${HERE}/../java/target/cldr-*-jar-with-dependencies.jar ${HERE}/../java/cldr.jar
--- data/cldr-tools-38.0/tools/scripts/updateHash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -x
-cd ../dist && shasum -a 512 cldr-*.{jar,zip} | tee SHASUM512.txt
